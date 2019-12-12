9a4e9b3dda3843b792aaf858cdd051a49b2a3c3e
ququzone <xueping.yang@gmail.com>
2019-12-13 00:38:25 +0800

    complete jwt verify

--- IoTeX.antenna.Tests/JWT/EK256KTests.cs
+++ IoTeX.antenna.Tests/JWT/EK256KTests.cs
@@ -34,5 +34,21 @@ namespace IoTeX.antenna.Tests.JWT
                 "eyJhbGciOiJFSzI1NksiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiIwNDRlMTgzMDZhZTllZjRlYzlkMDdiZjZlNzA1NDQyZDRkMWE3NWU2Y2RmNzUwMzMwY2EyZDg4MGYyY2M1NDYwN2M5YzMzZGViOWVhZTljMDZlMDZlMDRmZTljZTNkNDM5NjJjYzY3ZDVhYTM0ZmJlYjcxMjcwZDRiYWQzZDY0OGQ5IiwianRpIjoiNWQ3NmQyZTlmZjdjZjIzODUyMmVmNzFlIiwic3ViIjoiNWI3YTZkMjFkYzZlMzVlMTQ1NzRkMDUyIiwiZXhwIjoxNTc1ODQ0MzI5LCJpYXQiOjE1NjgwNjgzMjl9.rC2Vh6J-Xk0N2iHTfzuthVdejQtD_HnV770eLAMBGGgS4YZfW7F_i4pR2FusINtlXxhss5XKYL-NFYCuh_2N0gA",
                 signature);
         }
+        
+        [Fact]
+        public void TestJwtVerify()
+        {
+            var payload = new Dictionary<string, object>
+            {
+                {"iss", "044e18306ae9ef4ec9d07bf6e705442d4d1a75e6cdf750330ca2d880f2cc54607c9c33deb9eae9c06e06e04fe9ce3d43962cc67d5aa34fbeb71270d4bad3d648d9"},
+                {"jti", "5d76d2e9ff7cf238522ef71e"},
+                {"sub", "5b7a6d21dc6e35e14574d052"},
+                {"exp", 1575844329},
+                {"iat", 1568068329}
+            };
+
+            var parsedPayload = EK256K.Verify("eyJhbGciOiJFSzI1NksiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiIwNDRlMTgzMDZhZTllZjRlYzlkMDdiZjZlNzA1NDQyZDRkMWE3NWU2Y2RmNzUwMzMwY2EyZDg4MGYyY2M1NDYwN2M5YzMzZGViOWVhZTljMDZlMDZlMDRmZTljZTNkNDM5NjJjYzY3ZDVhYTM0ZmJlYjcxMjcwZDRiYWQzZDY0OGQ5IiwianRpIjoiNWQ3NmQyZTlmZjdjZjIzODUyMmVmNzFlIiwic3ViIjoiNWI3YTZkMjFkYzZlMzVlMTQ1NzRkMDUyIiwiZXhwIjoxNTc1ODQ0MzI5LCJpYXQiOjE1NjgwNjgzMjl9.rC2Vh6J-Xk0N2iHTfzuthVdejQtD_HnV770eLAMBGGgS4YZfW7F_i4pR2FusINtlXxhss5XKYL-NFYCuh_2N0gA");
+            Assert.Equal(payload["iss"], parsedPayload["iss"]);
+        }
     }
 }
--- IoTeX.antenna/JWT/EK256K.cs
+++ IoTeX.antenna/JWT/EK256K.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Text;
 using IoTeX.antenna.Accounts;
 using JWT;
 using JWT.Serializers;
+using Nethereum.Hex.HexConvertors.Extensions;
 
 namespace IoTeX.antenna.JWT
 {
@@ -35,6 +37,45 @@ namespace IoTeX.antenna.JWT
             var signature = algorithm.Sign(key, Encoding.ASCII.GetBytes(headerBase + "." + payloadBase));
 
             return headerBase + "." + payloadBase + "." + urlEncoder.Encode(signature);
+        }
+
+        public static Dictionary<string, object> Verify(string token)
+        {
+            IJsonSerializer serializer = new JsonNetSerializer();
+            IBase64UrlEncoder urlEncoder = new JwtBase64UrlEncoder();
+
+            var segments = token.Split('.');
+
+            if (segments.Length != 3)
+            {
+                throw new Exception("token invalid");
+            }
+            
+            var header = serializer.Deserialize<Dictionary<string, string>>(
+                System.Text.Encoding.UTF8.GetString(urlEncoder.Decode(segments[0])));
+            if (header["alg"] != "EK256K")
+            {
+                throw new Exception("alg should be EK256K but got " + header["alg"]);
+            }
+
+            
+            var payload = serializer.Deserialize<Dictionary<string, object>>(
+                System.Text.Encoding.UTF8.GetString(urlEncoder.Decode(segments[1])));
+            var signature = urlEncoder.Decode(segments[2]);
+
+            var empty = new Account();
+            var recoveredAddress = empty.Recover(segments[0] + "." + segments[1], signature, false);
+            
+            var secretOrPublicKey = payload["iss"].ToString();
+            var expectedAddress = Account.PublicKeyToAddress(secretOrPublicKey.HexToByteArray());
+            if (recoveredAddress != expectedAddress) 
+            {
+                throw new Exception(recoveredAddress + " signed the signature but we are expecting " + expectedAddress);
+            }
+            if (payload["iss"].ToString() != secretOrPublicKey) {
+                throw new Exception("issuer of the token does not match " + secretOrPublicKey);
+            }
+            return payload;
         }
     }
 }
